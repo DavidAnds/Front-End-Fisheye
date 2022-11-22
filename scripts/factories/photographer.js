ccfcdeffcb945c87e6f2463234851c2540e67a90
David ANDRIASON <davidandriason7@gmail.com>
2022-11-22 17:19:07 +0100

    change the photographer.js file to have all the necessary data on the dom

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,19 @@
 function photographerFactory (data) {
-  const { name, portrait } = data
+  const { name, portrait, city, country, tagline, price } = data
 
   const picture = `assets/photographers/${portrait}`
 
   function getUserCardDOM () {
     const article = document.createElement('article')
-    const img = document.createElement('img')
-    img.setAttribute('src', picture)
-    const h2 = document.createElement('h2')
-    h2.textContent = name
-    article.appendChild(img)
-    article.appendChild(h2)
-    return (article)
+    article.innerHTML = `
+    <img src="${picture}" alt="picture of ${name}" class="photographer_img">
+    <h2 class="photographer_name">${name}</h2>
+    <p class="photographer_location">${city}, ${country}</p>
+    <p class="photographer_tagline">${tagline}</p>
+    <p class="photographer_price">${price}€/jour</p>
+    `
+    article.className = 'photographer_item'
+    return article
   }
   return { name, picture, getUserCardDOM }
 }
